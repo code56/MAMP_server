dfcee8c3f8e20130308055460d2b20c270e11583
code56 <evanthiak432@gmail.com>
2016-06-24 17:24:09 +0100

    working php connection to db

---
+++ connect-mysql.php
@@ -0,0 +1,26 @@
+<?php
+
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PSWD', 'evanthia1011');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'InteractiveFigure');
+
+
+
+
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+
+
+
+if(!$con)
+	{
+		die ('Not Connected to database Server');
+	}
+
+echo 'you have connected successfully';
+
+
+
+	
+?>
