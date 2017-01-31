cba72430f40b3b8b2a0761f0e64d55bb1ab0b636
code56 <evanthiak432@gmail.com>
2017-01-31 16:35:23 +0000

    the current_path() function /node/1 works

--- drupal-7.43/sites/all/modules/custom/dataform/js/dataform_menu.js
+++
@@ -1,7 +0,0 @@
-//jQuery(document).ready(function () { alert("Hello from the ./js/dataform_menu.js!"); });
-
-(function ($) {
-  $(document).ready(function(){
-    alert("Hello world added js in the ./misc/dataform_menu.js!");  //here we can add our JS code
-  })
-})(jQuery);
